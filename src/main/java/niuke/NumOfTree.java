5defb32041f8657923527990b6aac015b35940a1
zuanjian.chen <zuanjian.chen@ymm56.com>
2016-08-10 21:54:47 +0800

    第三天（未完成）

---
+++ src/main/java/niuke/NumOfTree.java
@@ -0,0 +1,15 @@
+package niuke;
+
+import java.util.Scanner;
+
+/**
+ * Created by 陈钻健 on 2016/8/8.
+ */
+public class NumOfTree {
+
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        
+
+    }
+}
